ead4d38fd6044aea0cafbdc21ff4408a94762594
Jonathan Morais Barcellos Ferreira <jonathanmbferreira@outlook.com>
2024-01-20 20:39:01 -0300

    adiciona os arquivos

---
+++ SCRIPT/coleta.R
@@ -0,0 +1,35 @@
+# Criar um objeto para os dados do arquivo aije_tse.json
+
+dados <- jsonlite::read_json("DATA/aije_tse.json")
+
+## Criar uma tabela com todos os dados
+
+tabela <- purrr::map_dfr(seq_along(dados$content), ~ {
+  tabela <- data.frame(
+    cod_dec = dados$content[[.x]]$codigoDecisao,
+    data_dec = dados$content[[.x]]$dataDecisao,
+    sig_c = dados$content[[.x]]$siglaClasse,
+    desc_c = dados$content[[.x]]$descricaoClasse,
+    tipo_proc = dados$content[[.x]]$nomeTipoProcesso,
+    relator = dados$content[[.x]]$relator[[1]]$nome
+  )
+}, .progress = TRUE)
+
+# Filtra apenas as decisões a partir de 2018
+
+tabela <-
+  tabela |> dplyr::filter(
+    lubridate::year(data_dec) >= 2018
+  )
+
+## Download
+
+UA <- "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.51"
+
+purrr::walk(seq(1, nrow(tabela)), purrr::possibly(~ {
+	httr::GET(
+		paste0("https://sjur-servicos.tse.jus.br/sjur-servicos/rest/download/pdf/", tabela$cod_dec[[.x]]),
+		httr::add_headers("User-Agent" = UA),
+		httr::write_disk(paste0("DATA/DECISOES/", tabela$cod_dec[[.x]], ".pdf"), T)
+	)
+}, NULL, quiet = FALSE), .progress = list(type = "tasks"))
