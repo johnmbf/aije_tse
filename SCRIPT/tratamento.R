363a55fcba4897a6905ac996ad80a68f93f6b530
Jonathan Morais Barcellos Ferreira <jonathanmbferreira@outlook.com>
2024-01-20 22:23:11 -0300

    atualiza arquivos e melhora o codigo de tratamento

--- SCRIPT/tratamento.R
+++ SCRIPT/tratamento.R
@@ -23,20 +23,32 @@ purrr::walk2(pdf, word, ~ {
 
     # Excluir as linhas que representam o cabeçalho e o rodapé
     if (stringr::str_detect(.x, "_B") == TRUE) {
-      cabecalho <- 4
+      # arquivos tipo B tem apenas 4 linhas de cabeçalho
+      rodape <- 2
       linhas_tratadas <-
-        linhas[-c(1:cabecalho)]
+        linhas[-seq(
+          length(linhas),
+          length(linhas) - min(rodape, length(linhas))
+        )]
     } else if (stringr::str_detect(.x, "_A") == TRUE) {
+      # arquivos tipo A tem 2 linhas de rodapé
       rodape <- 1
       linhas_tratadas <-
         linhas[-seq(
           length(linhas),
           length(linhas) - min(rodape, length(linhas))
         )]
+    } else if (stringr::str_detect(.x, "_E") == TRUE) {
+      # arquivos tipo A tem 2 linhas de rodapé
+      cabecalho <- 4
+      linhas_tratadas <-
+        linhas[-c(1:cabecalho)]
     } else if (stringr::str_detect(.x, "_C") == TRUE) {
+      # arquivos tipo C tem 1 linha de cabeçalho
       linhas_tratadas <-
         linhas[-1]
     } else if (stringr::str_detect(.x, "_D") == TRUE) {
+      # arquivos tipo D tem 1 linha de rodapé
       linhas_tratadas <-
         linhas[-length(linhas)]
     }
@@ -59,3 +71,43 @@ purrr::walk2(pdf, word, ~ {
 }, .progress = list(type = "tasks"))
 
 # Importar para o R
+textos <- readtext::readtext(
+  "DATA/DECISOES/*.docx"
+) |>
+  dplyr::rename(cod_dec = doc_id)
+
+textos$cod_dec <- textos$cod_dec |>
+  stringr::str_extract('\\d+')
+
+# Remove tudo antes do voto (... é o relatório)
+textos$text <-
+  textos$text |>
+  stringr::str_remove(
+    "[\\s\\S]*É\\so\\srelatório"
+  )
+
+# Cria um corpus
+tse_corpus <- quanteda::corpus(
+  textos,
+  docid_field = "cod_dec"
+)
+
+# Cria tokens
+tse_tokens <-
+  quanteda::tokens(
+    tse_corpus,
+    remove_punct = T,
+    remove_symbols = T,
+    remove_url = T,
+    remove_separators = T,
+    remove_numbers = T,
+    split_hyphens = T
+  ) |>
+  quanteda::tokens_tolower()
+
+# Uniformiza através da criação de dicionários
+tse_tokens |>
+  decJ::dicionario.Criar(
+    n = 6,
+    'DATA/DIC/'
+  )
